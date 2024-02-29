026a4bb1e9e20cef02ee4732b3d08aa5fa2bade9
Angad Singh <singh04angad@gmail.com>
2024-02-29 23:04:33 +0530

    updated

--- java_factorial_fibonacci.java
+++ java_factorial_fibonacci.java
@@ -1,17 +1,18 @@
 public class java_factorial_fibonacci {
-    static int factorial(int n){
-        if(n==1){
+    static int factorial(int n) {
+        if (n == 1) {
             return 1;
         }
-        return n*factorial(n-1);
+        return n * factorial(n - 1);
     }
 
-    static int fibonacci(int x){
-        if(x<=1){
+    static int fibonacci(int x) {
+        if (x <= 1) {
             return x;
         }
-        return fibonacci(x-1)+fibonacci(x-2);
+        return fibonacci(x - 1) + fibonacci(x - 2);
     }
+
     public static void main(String[] args) {
         System.out.println(factorial(5));
         System.out.println(fibonacci(6));
