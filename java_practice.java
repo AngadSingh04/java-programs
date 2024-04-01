68aa07d997f254a776408349c50161e212a9a101
Angad Singh <singh04angad@gmail.com>
2024-04-01 19:20:16 +0530

    BigDecimal practice

---
+++ java_practice.java
@@ -0,0 +1,15 @@
+import java.math.BigDecimal;
+
+public class java_practice {
+    public static void main(String[] args) {
+        double a = 0.02;
+        double b = 0.03;
+        double c = b - a;
+        System.out.println(c);
+
+        BigDecimal a1 = new BigDecimal("0.02");
+        BigDecimal b1 = new BigDecimal("0.03");
+        BigDecimal c1 = b1.subtract(a1);
+        System.out.println(c1);
+    }
+}
