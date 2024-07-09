decacd215e399263ed10b301d4d28aa4b31060bd
AngadSingh04 <singh04angad@gmail.com>
2024-07-09 14:40:08 +0530

    protected access specifier

---
+++ Java 2/yeye.java
@@ -0,0 +1,12 @@
+class Dance{
+    protected void show(){
+        System.out.println("I am Dancing");
+    }
+}
+public class yeye {
+    public static void main(String[] args) {
+        System.out.println("yeyeyeye");
+        Dance d1 = new Dance();
+        d1.show();
+    }
+}
