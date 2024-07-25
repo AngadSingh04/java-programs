5b028ee6c4cac70ecf402673a6cff199d6cb467b
AngadSingh04 <singh04angad@gmail.com>
2024-07-25 14:18:25 +0530

    anonymous class updated

--- AnonymousE.java
+++ AnonymousE.java
@@ -19,12 +19,17 @@
 //     }
 // }
 
-interface Cycle{
-    public void gearUp();
-    public void start();
+class Cycle{
+    public void gearUp(){
+        System.out.println("Gear is starting....");
+    }
+    public void start(){
+
+    }
 }
-public class AnonymousE {
-    public static void main(String[] args) {
+class ECycle{
+    public void display(){
+        System.out.println("Electric Cycle is getting displayed.");
         Cycle hero = new Cycle(){
             public void gearUp(){
                 System.out.println("This is anonymous cycle gear");
@@ -35,6 +40,12 @@ public class AnonymousE {
         };
         hero.gearUp();
         hero.start();
+    }
+}
+public class AnonymousE {
+    public static void main(String[] args) {
+        ECycle atlas = new ECycle();
+        atlas.display();
     }
 }
 
