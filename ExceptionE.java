7fdbca822f15f1c5cb5d00ea56eba81e958e6f54
AngadSingh04 <singh04angad@gmail.com>
2024-08-01 13:48:56 +0530

    updated

--- ExceptionE.java
+++ ExceptionE.java
@@ -1,11 +1,15 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 public class ExceptionE {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException{
         try {
             // long[] a = new long[Integer.MAX_VALUE]; // out of memory error
             // String name = null;
             // System.out.println(name.length()); // Null Pointer Exception
-            int[] a = {1,2,3,4,5};
-            System.out.println(a[6]); // ArrayIndexOutOfBoundsException
+            // int[] a = {1,2,3,4,5};
+            // System.out.println(a[6]); // ArrayIndexOutOfBoundsException
+            FileReader fr = new FileReader("file.txt");
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
