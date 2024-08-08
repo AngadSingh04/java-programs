c7cc7a732647afed604ef5c9c3e369f1706a43b4
AngadSingh04 <singh04angad@gmail.com>
2024-08-08 14:10:02 +0530

    CollectionsDemo.java

---
+++ CollectionsDemo.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class CollectionsDemo {
+    public static void main(String[] args) {
+        // ArrayList<String> list1 = new ArrayList<>();
+        List<String> list1 = new ArrayList<>();
+        list1.add("Angad");
+        list1.add("junior");
+        list1.add("haya");
+        list1.add("ola");
+        System.out.println(list1);
+        Collections.sort(list1);
+        System.out.println(list1);
+        Collections.reverse(list1);
+        System.out.println(list1);
+        Collections.shuffle(list1);
+        System.out.println(list1);
+
+    }
+}
